43bcdc87c870244f85691ae9188d25b57f83ce85
ankitmr27 <ankitm.ac.in@gmail.com>
2024-10-10 23:05:39 +0530

    added db migration scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,72 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+// import { MiddlewareConsumer, Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { MiddlewareUser } from './middleware/extractUserInfo.middleware';
+// import { AuthModule } from './modules/auth/auth.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { jwtModule } from './modules/jwt /jwt.module';
+// import { PostModule } from './modules/post/post.module';
+// import { CommentModule } from './modules/comment/comment.module';
+// import { User } from './Entity/User.entity';
+
+// @Module({
+//   imports: [
+//     AuthModule,
+//     jwtModule,
+//     PostModule,
+//     CommentModule,
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: process.env.POSTGRES_HOST,
+//       port: Number(process.env.POSTGRES_PORT),
+//       username: process.env.POSTGRES_USER,
+//       password: process.env.POSTGRES_PASSWORD,
+//       database: process.env.POSTGRES_DB,
+//       synchronize: false, // Be cautious with this in production set false
+//       // update db schema according to current entity schemas
+//       logging: true,
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       ssl: false, // TODO : we need to add ssl certifcation
+//     }),
+//     TypeOrmModule.forFeature([User]),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService, MiddlewareUser],
+// })
+// export class AppModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(MiddlewareUser).forRoutes('/');
+//   }
+// }
+
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MiddlewareUser } from './middleware/extractUserInfo.middleware';
-import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { jwtModule } from './modules/jwt /jwt.module';
+import typeorm from './config/tyeporm';
+import { AuthModule } from './modules/auth/auth.module';
+import { jwtModule } from './jwt /jwt.module';
 import { PostModule } from './modules/post/post.module';
 import { CommentModule } from './modules/comment/comment.module';
-import { User } from './Entity/User.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
     AuthModule,
     jwtModule,
     PostModule,
     CommentModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      synchronize: false, // Be cautious with this in production set false
-      // update db schema according to current entity schemas
-      logging: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      ssl: false, // TODO : we need to add ssl certifcation
-    }),
-    TypeOrmModule.forFeature([User]),
   ],
   controllers: [AppController],
-  providers: [AppService, MiddlewareUser],
+  providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(MiddlewareUser).forRoutes('/');
-  }
-}
+export class AppModule {}
---
+++ src/migrations/1728578595392-first.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class First1728578595392 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
